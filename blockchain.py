1aa387713482b2eacb370a689cedaf4aeead9244
Nutod <alukodotun@gmail.com>
2019-10-10 12:20:17 +0700

    Updated initializer variables

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,12 @@
-# Initializing variables
-GENESIS_BLOCK = {}
+# Initialize the Variables
+MINING_REWARD = 10
+GENESIS_BLOCK = {
+  'previous_hash': 'XYZ',
+  'index': 0,
+  'transactions': [],
+}
 
-block = list()
+blockchain = list(GENESIS_BLOCK)
 open_transactions = list()
+owner = 'Nutod'
+participants = {'Nutod'}
