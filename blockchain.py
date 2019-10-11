0f87c1187ecdbad6dfcf735ab92bc0a779f5d1a9
Nutod <alukodotun@gmail.com>
2019-10-11 21:09:58 +0700

    Added new mine_block logic to main blockchain file

--- blockchain.py
+++ blockchain.py
@@ -15,3 +15,27 @@ participants = {'Nutod'}
 # TODO: Add this to an utils module
 def hash_block(block):
   return '-'.join([str(block[key]) for key in block])
+
+def mine_block():
+  last_block = blockchain[-1]
+  hashed_block = '-'.join([str(last_block[key]) for key in last_block])
+  reward_transaction = {
+    'sender': 'MINING',
+    'recipient': owner,
+    'amount': MINING_REWARD
+  }
+  copied_transactions = open_transactions[:]
+  copied_transactions.append(reward_transaction)
+  # hashed_block = str([last_block[key] for key in last_block])
+  # for key in last_block:
+  #   value = last_block[key]
+  #   hashed_block = hashed_block + str(value)
+    
+  block = {
+    'previous_hash': hashed_block,
+    'index': len(blockchain),
+    'transactions': copied_transactions,
+  }
+
+  blockchain.append(block)
+  return True
